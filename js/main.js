a9b059c389ffef995a74e942183f24606487960b
Antonina <tonyaberch@gmail.com>
2021-01-31 19:42:54 +0100

    fix: reduce the code to one function getRandomNumber()

--- js/main.js
+++ js/main.js
@@ -1,23 +1,7 @@
-const getRandomInt = (min, max) =>{
-  if (max > min && min >= 0) {
-    min = Math.floor(min);
-    max = Math.floor(max);
-    return Math.round(Math.random() * (max - min) + min);
+const getRandomNumber = (min, max, numberOfDigits = 0) => {
+  if (max >= min && min >= 0) {
+    let numberOfSigns = 10 ** numberOfDigits;
+    return Math.round((Math.random() * (max - min) + min) * numberOfSigns) / numberOfSigns;
   }
-  else{
-    return null;
-  }
+  throw new Error('Число меньше нуля');
 }
-const getRandomFloat = (min, max, numberOfDigits)=>{
-  if (max > min && min >= 0) {
-    let numberOfSigns = 1;
-    for (let i = 0; i < numberOfDigits; i++){
-      numberOfSigns *= 10;
-    }
-    return Math.round((Math.random() * (max - min) + min)*numberOfSigns)/numberOfSigns;
-  }
-  else{
-    return null;
-  }
-}
-
